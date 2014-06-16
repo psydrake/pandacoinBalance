8631c7f09120ca94d739b5810b1765c577b72979
Drake Emko <demko@revionics.com>
2014-06-16 00:24:18 -0600

    using backup URLs for certain trading pairs (in case of failure of primary URL)

--- appengine/pandacoinBalance/main.py
+++ appengine/pandacoinBalance/main.py
@@ -18,6 +18,10 @@ from server.controllers import RESOURCE_NAME_controller
 BLOCKEXPLORER_URL = 'http://pandachain.net/chain/PandaCoin/q/addressbalance/'
 BLOCKEXPLORER_URL_BACKUP = 'http://pnd.showed.us/chain/PandaCoin/q/addressbalance/'
 TRADING_PAIR_URL = 'http://www.cryptocoincharts.info/v2/api/tradingPair/'
+TRADING_PAIR_URL_BTC_BACKUP="https://api.mintpal.com/v1/market/stats/PND/" # also used for LTC
+TRADING_PAIR_URL_USD_BACKUP = 'https://coinbase.com/api/v1/prices/buy' 
+# TRADING_PAIR_URL_FIAT_BACKUP = 'http://api.bitcoincharts.com/v1/markets.json'
+
 TIMEOUT_DEADLINE = 10 # seconds
 
 # Run the Bottle wsgi application. We don't need to call run() since our
@@ -102,10 +106,41 @@ def tradingPND(currency='BTC'):
 
 def pullTradingPair(currency1='PND', currency2='BTC'):
     url = TRADING_PAIR_URL + currency1 + '_' + currency2
-    data = urlfetch.fetch(url, deadline=TIMEOUT_DEADLINE)
+    data = None
+    useBackupUrl = False
+
+    try:
+        data = urlfetch.fetch(url, deadline=TIMEOUT_DEADLINE)
+    except:
+        logging.warn('Error retrieving ' + url)
+        useBackupUrl = True
+
+    if (useBackupUrl):
+        if (currency1 == 'PND' and currency2 in ['BTC', 'LTC']):
+            backupUrl = TRADING_PAIR_URL_BTC_BACKUP + currency2
+            logging.warn('Now trying ' + backupUrl)
+            data = urlfetch.fetch(backupUrl, deadline=TIMEOUT_DEADLINE)
+        elif (currency1 == 'BTC' and currency2 == 'USD'):
+            backupUrl = TRADING_PAIR_URL_USD_BACKUP
+            logging.warn('Now trying ' + backupUrl)
+            data = urlfetch.fetch(backupUrl, deadline=TIMEOUT_DEADLINE)
+        else:
+            logger.error('Cannot get trading pair for ' + currency1 + ' / ' + currency2)
+            return
 
     dataDict = json.loads(data.content)
-    tradingData = json.dumps(dataDict)
+    if (useBackupUrl):
+        if (currency1 == 'PND' and currency2 in ['BTC', 'LTC']):
+            dataDict = {'price': dataDict[0]['last_price']}
+        elif (currency1 == 'BTC' and currency2 == 'USD'):
+            if (dataDict['subtotal']['currency'] == 'USD'):
+                dataDict = {'price': dataDict['subtotal']['amount']}
+            else:
+                logger.error('Unexpected JSON returned from URL ' + TRADING_PAIR_URL_USD_BACKUP)
+        else:
+            logger.error('Should never get here')
+
+    tradingData = json.dumps(dataDict).strip('"')
 
     memcache.set('trading_' + currency1 + '_' + currency2, tradingData)
     logging.info('Stored in memcache for key trading_' + currency1 + '_' + currency2 + ': ' + tradingData)
@@ -114,9 +149,9 @@ def pullTradingPair(currency1='PND', currency2='BTC'):
 def pullCryptocoinchartsData():
     pullTradingPair('PND', 'BTC')
     pullTradingPair('PND', 'LTC')
+    pullTradingPair('BTC', 'USD')
     pullTradingPair('BTC', 'CNY')
     pullTradingPair('BTC', 'EUR')
-    pullTradingPair('BTC', 'USD')
     return "Done"
 
 @bottle.error(404)
